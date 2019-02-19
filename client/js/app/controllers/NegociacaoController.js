d6b7abb063e1aba2657140d5b7fc2ff772b882a2
Ana Alice <anaalicd.cd@gmail.com>
2019-02-19 20:35:06 -0300

    final do curso2

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,62 @@
+class NegociacaoController {
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this._inputQuantidade = $("#quantidade");
+        this._inputData = $("#data");
+        this._inputValor = $("#valor");
+
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(),
+            new NegociacoesView($("#negociacoes-view")),
+            'adiciona','esvazia');
+        this._mensagem = new Bind(
+            new Mensagem(),
+            new MensagemView($("#negociacoes-mensagem")),'texto');
+    }
+
+
+
+    adiciona(event) {
+        event.preventDefault();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._mensagem.texto = "Negociacoes atualizadas com sucesso";
+        this._limpaFormulario();
+
+    }
+    apaga() {
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = "Negociações apagadas com sucesso";
+
+    }
+    importaNegociacoes(){
+        let service = new NegociacaoService();
+        
+        Promise.all([
+            service.obterNegociacoesDaSemana(),
+            service.obterNegociacoesDaSemanaAnterior(),
+            service.obterNegociacoesDaSemanaRetrasada()]
+        ).then(negociacoes =>{
+            negociacoes
+                .reduce((arrayAchatado,array) => arrayAchatado.concat(array),[])
+                .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = "Negociacoes Adicionadas com Sucesso"
+        }).catch(erro => this._mensagem.texto = erro);
+
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
+    }
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
+
+    }
+}
